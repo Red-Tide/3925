c5466663d7afd42cafa63606efe0009659ff6e41
Si-Yang Wu <siyang7@hotmail.com>
2017-07-01 23:06:47 -0700

    july 1

--- connect.php
+++ connect.php
@@ -1,15 +1,15 @@
 <?php
-$con = mysql_connect("localhost","root","");
-$connect = mysql_select_db("lights",$con);
+$con = mysql_connect("localhost", "root", "");
+$connect = mysql_select_db("lights", $con);
 mysql_query("set names gb2313");
 
 
 if(!$connect)
 {
-	die('Could not connet: ' . mysql_error());
+	die('Could not connect: ' . mysql_error());
 }
 
-echo 'connect succesful';
+echo 'connect successful';
 
 
 ?>

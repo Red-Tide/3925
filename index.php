d5ad879e1d6ffdd3b7e787c4dde635938c3b0cbb
Si-Yang <siyang7@hotmail.com>
2017-07-13 14:13:59 -0700

    changes to star menu

--- index.php
+++ index.php
@@ -116,8 +116,8 @@ else{
         </a>
 	    
       <ul id="wheel" data-angle="all">
-        <li class="item"><a href="#home"><img src="images/star1.png" width="100%" height="100%" onclick="smallStarSound()" /></a></li>
-        <li class="item"><a href="#home"><img src="images/star2.png" width="100%" height="100%" onclick="smallStarSound()" /></a></li>
+        <li class="item"><a href="#home"><img src="images/blue_star.png" width="100%" height="100%" onclick="smallStarSound()" /></a></li>
+        <li class="item"><a href="#home"><img src="images/blue_star2.png" width="100%" height="100%" onclick="smallStarSound()" /></a></li>
         <li class="item"><a href="#home"><img src="images/star3.png" width="100%" height="100%" onclick="smallStarSound()" /></a></li>
         <li class="item"><a href="#home"><img src="images/star4.png" width="100%" height="100%" onclick="smallStarSound()" /></a></li>
       </ul>

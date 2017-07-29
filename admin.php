19b94476076c29ff010bc88b1325e33925365949
Si-Yang <siyang7@hotmail.com>
2017-07-29 16:53:38 -0700

    sfklk

--- admin.php
+++ admin.php
@@ -1,34 +1,36 @@
 <?php
 session_start();
 ?>
+
 <!DOCTYPE html>
 <html>
-<body>
-<h1>Lights of Hope: Administration</h1>
-    <form method="post" name="login">
-        <label>Admin:</label>
-        <input type="text" name="admin"/>
-        <br />
-        <br />
-        <label>Password:</label>
-        <input type="password" name="passwd"/>
-        <br />
-        <br />
-<button type="submit" onclick="submit">Login</button>
-    </form>
-        </body>
+    <body>
+        <h1>Lights of Hope: Administration</h1>
+        <form method="post" name="login">
+            <label>Admin:</label>
+            <input type="text" name="admin"/>
+            <br />
+            <br />
+            <label>Password:</label>
+            <input type="password" name="passwd"/>
+            <br />
+            <br />
+            <button type="submit" onclick="submit">Login</button>
+        </form>
+    </body>
 </html>
+
 <?php
-if(isset($_POST['admin']) && $_POST['passwd']){
-if ($_POST['admin'] == "lhope" && $_POST['passwd'] == "pumpkinpie99"){
-    $_SESSION['login'] = true;   
-}
-echo $_POST['admin'];
-echo "\n";
-echo $_POST['passwd'];
+if (isset($_POST['admin']) && $_POST['passwd']) {
+    if ($_POST['admin'] == "lhope" && $_POST['passwd'] == "pumpkinpie99") {
+        $_SESSION['login'] = true;   
+    }
+    //echo $_POST['admin'];
+    //echo "\n";
+    //echo $_POST['passwd'];
 }
     
-if(isset($_SESSION['login'])){
+if (isset($_SESSION['login'])) {
     header("location: panel.php");
 }
 ?>
--- panel.php
+++ panel.php
@@ -2,7 +2,7 @@
 session_start();
 
 if (!isset($_SESSION['login'])) {
-  header('Location: admin.php');
+    header('Location: admin.php');
 }
 extract($_POST);
 
@@ -10,33 +10,34 @@ extract($_POST);
 
 <!DOCTYPE html>
 <html>
-<body>
-
-<h1>Administration</h1>
-<form method="post" name="adminpanel">
-<button type="submit" name="restart" onclick="restart">Restart Queue</button>
-<button type="submit" name="get_data" onclick="get_data">Get Spreadsheet</button>
-<button type="submit" name="logout" onclick="logout">Logout</button>
-</form>
-<hr />
-</body>
+    <body>
+        <head>
+            <title>Admin</title>
+        </head>
+        <h1>Administration</h1>
+        <form method="post" name="adminpanel">
+            <button type="submit" name="restart" onclick="restart">Restart Queue</button>
+            <button type="submit" name="get_data" onclick="get_data">Get Spreadsheet</button>
+            <button type="submit" name="logout" onclick="logout">Logout</button>
+        </form>
+        <hr />
+    </body>
 </html>
 
 <?php
 #redirect to login screen if logout button pressed
 
 if (isset($_POST['restart'])) {
-  echo "The Queue has been Restarted";
-  exec ('killall nodejs; nodejs server.js &; disown');
+    echo "The Queue has been Restarted";
+    shell_exec ('killall nodejs; nodejs server.js &; disown');
+    shell_exec ('sudo echo blah > /var/www/some.txt');
 }
 
 if (isset($_POST['get_data'])) {
-  echo "Grabbing the Database";
-  exec('python create-spread.py');
-  header("location:lightsofhope.xlsx");
+    echo "Grabbing the Database";
+    exec('/usr/bin/python /var/www/html/create-spread.py');
+    //header("location:lightsofhope.xlsx");
 }
-
-
 
 if (isset($_POST['logout'])) {
     session_destroy();
